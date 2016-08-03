11eb40f190cbac447fe2ff9cb93e628f110d7fa7
ataw <ataw@atawdemac-mini.local>
2016-08-03 16:33:40 +0800

    Initial Commit

---
+++ EncryptAlbumMeno/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EncryptAlbumMeno
+//
+//  Created by ataw on 16/8/3.
+//  Copyright © 2016年 王宗成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
